50cf14279f93b5ea1dfb1fda716bc36c85a01d17
Run <run2gotv@gmail.com>
2024-02-08 21:42:11 +0100

    init

---
+++ src/ButtonIds.h
@@ -0,0 +1,37 @@
+#pragma once
+/*
+#define BASE_ID_NUMBERS 1000
+#define NUMBER_1 BASE_ID_NUMBERS + 1
+*/
+
+enum ButtonIds
+{
+	// Base 10
+	NUM_0,
+	NUM_1,
+	NUM_2,
+	NUM_3,
+	NUM_4,
+	NUM_5,
+	NUM_6,
+	NUM_7,
+	NUM_8,
+	NUM_9,
+	// Base 16
+	NUM_A,
+	NUM_B,
+	NUM_C,
+	NUM_D,
+	NUM_E,
+	NUM_F,
+
+	// Operators
+	OP_ADD,
+	OP_SUB,
+	OP_MUL,
+	OP_DIV,
+
+	// Extra Buttons
+	BT_EVAL,
+	BT_COMMA
+};
